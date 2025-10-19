959a72aa13c84be7b22d7d926e6bd82f459a97b6
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-10-19 11:25:04 +0530

    refactor:teamrepo datatype

--- src/main/java/com/example/RaceHub/repository/TeamRepository.java
+++ src/main/java/com/example/RaceHub/repository/TeamRepository.java
@@ -1,7 +1,9 @@
 package com.example.RaceHub.repository;
 
+import com.example.RaceHub.models.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeamRepository extends JpaRepository {
+public interface TeamRepository extends JpaRepository<Team,Long> {
+
 
 }
