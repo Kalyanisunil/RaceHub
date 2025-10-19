190919dff0cd46a59df561951c285fc34152bb53
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-10-19 11:21:08 +0530

    feat:teamRepository

--- src/main/java/com/example/RaceHub/models/Team.java
+++ src/main/java/com/example/RaceHub/models/Team.java
@@ -1,4 +1,75 @@
 package com.example.RaceHub.models;
 
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String teamName;
+    private String teamColor;
+    private String country;
+
+    @OneToMany(mappedBy = "team")
+    private List<Driver> drivers;
+
+    public Team() {
+
+    }
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<Driver> getDrivers() {
+        return drivers;
+    }
+
+    public void setDrivers(List<Driver> drivers) {
+        this.drivers = drivers;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTeamColor() {
+        return teamColor;
+    }
+
+    public void setTeamColor(String teamColor) {
+        this.teamColor = teamColor;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public Team(String country, List<Driver> drivers, Long id, String teamColor, String teamName) {
+        this.country = country;
+        this.drivers = drivers;
+        this.id = id;
+        this.teamColor = teamColor;
+        this.teamName = teamName;
+    }
+
 }
+
---
+++ src/main/java/com/example/RaceHub/repository/TeamRepository.java
@@ -0,0 +1,7 @@
+package com.example.RaceHub.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository {
+
+}
